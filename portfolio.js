e300dd5ac5926a812c2428f7df151e6b0731aab8
Joshtg1104 <joshtg1104@gmail.com>
2019-02-14 12:05:44 -0600

    so far so good

---
+++ portfolio.js
@@ -0,0 +1,11 @@
+// let allow = false;
+// $("iframe").on('mouseover', see);
+// function see() {
+//     if(allow) {
+//         $("iframe").on('mouseout', 'disable');
+//         allow = true;
+//     }
+//     else{
+//         $("iframe").remove('disable')
+//     }
+// }
