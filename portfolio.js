1e7edfe755ac0cfdf0ba417c55dda625c304b783
Joshtg1104 <joshtg1104@gmail.com>
2019-02-15 16:17:16 -0600

    Can't wait to present

--- portfolio.js
+++ portfolio.js
@@ -1,11 +1,19 @@
-// let allow = false;
-// $("iframe").on('mouseover', see);
-// function see() {
-//     if(allow) {
-//         $("iframe").on('mouseout', 'disable');
-//         allow = true;
-//     }
-//     else{
-//         $("iframe").remove('disable')
-//     }
-// }
+// let reveal = document.getElementsByClassName('hitMeUp');
+// console.log(reveal);
+// reveal[0].onmouseover=function() {
+//     reveal.removeAttr('hidden')
+// };
+
+
+let reveal = document.getElementsByClassName('hitMeUp');
+let notHidden = document.getElementsByClassName('notHiddenArea');
+
+
+notHidden[0].onmouseover=function() {
+    reveal[0].removeAttribute('hidden');
+    reveal[0].classList.add("animated", "pulse");
+};
+
+// notHidden[0].addEventListener("mouseover", function(){
+//     reveal[0].removeAttribute('hidden')
+// });
